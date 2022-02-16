a2d6fbfc858b4c7c2b301b309fb1e13b135cb75b
Anjikapatra <89796909+Anjikapatra@users.noreply.github.com>
2022-02-16 14:08:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function go_to_shawn_mendes()
+{
+    window.location.href="https://en.wikipedia.org/wiki/Shawn_Mendes";
+}
+function go_to_ariana_grande
+{
+    window.location.href="https://en.wikipedia.org/wiki/Ariana_Grande";
+}
